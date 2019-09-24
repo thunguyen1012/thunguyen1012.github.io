e28b4d2737e080079b89f6726ec20a9b4e2a44b5
thuqnguyen <thuqnguyen@kms-technology.com>
2019-09-24 17:37:52 +0700

    min css

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,18 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   grunt.initConfig({
-      uncss: {
-          dist: {
-              files: [
-                  { src: 'index.html', dest: 'css/theme.css' }
-              ]
-          }
-      },
-      cssmin: {
-          dist: {
-              files: [
-                  { src: 'css/theme.css', dest: 'css/theme.css' }
-              ]
-          }
+    cssmin: {
+      dist: {
+        files: [
+            { src: 'css/theme.css', dest: 'css/theme.css' },
+            { src: 'css/theme-dark.css', dest: 'css/theme-dark.css' }
+        ]
       }
+    }
   });
 
   // Load the plugins
-  grunt.loadNpmTasks('grunt-uncss');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default tasks.
-  grunt.registerTask('default', ['uncss', 'cssmin']);
+  grunt.registerTask('default', ['cssmin']);
 };
